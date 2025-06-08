21e270d560c0b522f47b268c061ff84bee228459
huanganfree <huangan1206@163.com>
2025-06-08 10:50:32 +0800

    feat: 完成孔洞功能

--- src/model/ExtrudeGeometry.ts
+++ src/model/ExtrudeGeometry.ts
@@ -10,15 +10,25 @@ shape.lineTo(0, 200);
 shape.lineTo(150, 300);
 shape.lineTo(300, 200);
 shape.lineTo(300, 0);
+
+// 孔洞（必须在生成几何体之前添加到shape上）
+const hole = new THREE.Path();
+hole.moveTo(100, 50);
+hole.lineTo(200, 50);
+hole.lineTo(200, 100);
+hole.lineTo(100, 100);
+shape.holes.push(hole);
+
 const geometry = new THREE.ExtrudeGeometry(shape, { depth: 10, bevelEnabled: true });
 const material = new THREE.MeshBasicMaterial({
     color: new THREE.Color('lightgrey'),
     side: THREE.DoubleSide,
 });
-const houseWallGeometryMesh = new THREE.Mesh(geometry, material);
+const sideWall = new THREE.Mesh(geometry, material);
 
-const sideWall2 = houseWallGeometryMesh.clone()
+const sideWall2 = sideWall.clone()
 sideWall2.translateZ(300)
+
 
 // 房子的底板
 const geometryBottom = new THREE.BoxGeometry(300, 10, 300);
@@ -27,7 +37,7 @@ geometryBottom.translate(150, 5, 150); // 将底板移动到合适的位置
 const materialBottom = new THREE.MeshBasicMaterial({ color: new THREE.Color('grey') });
 const houseBottomGeometryMesh = new THREE.Mesh(geometryBottom, materialBottom);
 
-houseGroup.add(houseWallGeometryMesh);
+houseGroup.add(sideWall);
 houseGroup.add(sideWall2);
 houseGroup.add(houseBottomGeometryMesh);
 
